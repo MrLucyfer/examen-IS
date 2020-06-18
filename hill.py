e43c4602a344e6f45573438716175cafaa4e5075
Nil Folquer <nfolquer@gmail.com>
2020-06-18 18:24:26 +0200

    Improvements

--- hill.py
+++ hill.py
@@ -17,14 +17,24 @@ args = parser.parse_args()
 key = np.array([(ord(letter) - 65 ) for letter in args.k]).reshape((2,2))
 
 def encode(message, key):
-    msg = np.array([ord(letter) - 65 for letter in message]).reshape((3,2))
-    multiplication = np.dot(msg, key) % len(dictionary)
+    msg = np.array([ord(letter) - 65 for letter in message]).reshape((2, 3))
+    multiplication = np.dot(key, msg) % len(dictionary)
     result = multiplication.flatten()
+    # print('Key: {}'.format(key))
+    # print(result)
     return ''.join([dictionary[num] for num in result])
+
+def decode(message, key):
+    inv_key = np.asmatrix(key)
+    inv_key = inv_key.H
+    det = np.linalg.det(key)
+    inv_key = inv_key / det
+    print(inv_key)
+    return encode(message, key)
 
 if args.c:
     print(encode(args.message, key))
 elif args.d:
-    print('Not done yet')
+    print(decode(args.message, key))
 elif args.b:
     print('Still not implemented sorry bois')
